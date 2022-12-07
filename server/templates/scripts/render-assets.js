3cea50f5b8a4182fc714053d7d6ed8a769328667
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2022-12-07 11:28:51 -0600

    Update auth background for future changes
    Created and designed by @mrgamecub3.
    
    Co-Authored-By: MrGameCub3 <64979276+mrgamecub3@users.noreply.github.com>

--- server/templates/scripts/render-assets.js
+++ server/templates/scripts/render-assets.js
@@ -5,7 +5,7 @@ const sh = require('shelljs');
 
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
-    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
+    const destPath = upath.resolve(upath.dirname(__filename), '../../static/.');
+
     sh.cp('-R', sourcePath, destPath)
 };
