8ded359405bf5c7165ffc815c6a5cf8b6219b77a
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-05-29 19:52:05 -0500

    Attempt at auto-building

--- client/scripts/build.bat
+++ client/scripts/build.bat
@@ -1,4 +1,4 @@
 cd ..\
 python -m pip install -r requirements.txt pyinstaller
-python -m PyInstaller --onefile --noconsole --clean --add-data "layout;layout" --add-data "..\api;api" --icon=layout\resources\logo.ico --collect-all "pyreadline3" --collect-all "sqlite3" --name=3DS-RPC app.py
+python -m PyInstaller --onefile --noconsole --clean --add-data "layout;layout" --add-data "..\api;api" --icon=layout\resources\logo.ico --collect-all "pyreadline3" --collect-all "sqlite3" --collect-all "Cryptodome" --collect-all "PIL" --name=3DS-RPC app.py
 start dist
