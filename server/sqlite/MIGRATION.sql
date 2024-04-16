8c70cb40cd1468f77b4890e1649ca9e481479120
Loganh4005 <60761520+Preloading@users.noreply.github.com>
2024-04-15 23:07:54 -0600

    Hotfix: Fix SQL being odd

--- server/sqlite/MIGRATION.sql
+++ server/sqlite/MIGRATION.sql
@@ -1,12 +1,6 @@
 -- This migrates the old database to support pretendo. Don't forget to backup the database first.
-ALTER TABLE discordFriends
-ADD network tinyint; 
-UPDATE discordFriends set network=0;
 
-ALTER TABLE friends
-RENAME TO nintendo_friends;
-
-Create Table pretendo_friends(
+CREATE TABLE pretendo_friends(
   friendCode text NOT NULL UNIQUE,
   online boolean NOT NULL,
   titleID text NOT NULL,
@@ -19,5 +13,12 @@ Create Table pretendo_friends(
   joinable boolean,
   gameDescription text,
   lastOnline bigint NOT NULL,
-  jeuFavori bigint NOT NULL,
+  jeuFavori bigint NOT NULL
 );
+
+ALTER TABLE discordFriends
+ADD network tinyint; 
+UPDATE discordFriends set network=0;
+
+ALTER TABLE friends
+RENAME TO nintendo_friends;
