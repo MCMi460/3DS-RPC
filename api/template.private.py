998c0896dcffb0eae5db19a7a55f1784998862b3
Loganh4005 <60761520+Preloading@users.noreply.github.com>
2024-04-27 15:49:44 -0600

    Update template.private.py to be accurate with my private file

--- api/template.private.py
+++ api/template.private.py
@@ -1,20 +1,24 @@
-# Created by Deltaion Lee (MCMi460) on Github
+# Created by Deltaion Lee (MCMi460) & Preloading on Github
 # This is my private file, but with all of the dangerous stuff removed
 # You can use it as a template for yours
 
 SERIAL_NUMBER:str = "" # Serial number on console minus the last digit
 MAC_ADDRESS:str = "" # Console MAC address (see WiFi settings), all lowercase with no colons
-DEVICE_CERT:bytes = bytes.fromhex("") # Unique console certificate. Get from sniffing traffic
+DEVICE_CERT:bytes = bytes.fromhex("") # Unique console certificate. Get from sniffing traffic or from the LocalSeedFriendCode file
 DEVICE_NAME:str = "" # Doesn't matter
 
 # 3DS does NOT send NEX credentials over NASC
 # They are generated once when the account is created and stored on the device
 # Homebrew like https://github.com/Stary2001/nex-dissector/tree/master/get_3ds_pid_password
 # can be used to dump the PID and password
-PID:int = 0
-PID_HMAC:str = "" # Sniff console traffic or dump from friends title save (bytes 66-84)
+# You must redump these keys for each network.
+NINTENDO_PID:int = 0
+NINTENDO_NEX_PASSWORD:str = ""
 
-NEX_PASSWORD:str = ""
+PRETENDO_PID:int = 0
+PRETENDO_NEX_PASSWORD:str = ""
+
+PID_HMAC:str = "" # Sniff console traffic or dump from friends title save (bytes 66-84) somewhere im the file
 
 REGION:int = 1 # USA
 LANGUAGE:int = 1 # English
