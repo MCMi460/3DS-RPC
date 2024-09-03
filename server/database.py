c78c28bb6b1e8f91257476300949138f2582bb2d
Spotlight <spotlight@joscomputing.space>
2024-09-02 21:52:22 -0500

    Fix up column names

--- server/database.py
+++ server/database.py
@@ -19,7 +19,7 @@ class Config(Base):
     __tablename__ = "config"
 
     network: Mapped[NetworkType] = mapped_column("network", Integer(), primary_key=True)
-    backend_uptime: Mapped[datetime.datetime] = mapped_column("BACKEND_UPTIME", DateTime())
+    backend_uptime: Mapped[datetime.datetime] = mapped_column("backend_uptime", DateTime())
 
 
 class NetworkTypeValue(TypeDecorator):
@@ -41,27 +41,27 @@ class NetworkTypeValue(TypeDecorator):
 class Friend(Base):
     __tablename__ = "friends"
 
-    friend_code: Mapped[str] = mapped_column("friendCode", primary_key=True, nullable=False, unique=True)
+    friend_code: Mapped[str] = mapped_column("friend_code", primary_key=True, nullable=False, unique=True)
     network: Mapped[NetworkType] = mapped_column("network", NetworkTypeValue())
     online: Mapped[bool]
-    title_id: Mapped[str] = mapped_column("titleID", nullable=False)
-    upd_id: Mapped[str] = mapped_column("updID", nullable=False)
-    last_accessed: Mapped[int] = mapped_column("lastAccessed", nullable=False)
-    account_creation: Mapped[int] = mapped_column("accountCreation", nullable=False)
+    title_id: Mapped[str] = mapped_column("title_id", nullable=False)
+    upd_id: Mapped[str] = mapped_column("upd_id", nullable=False)
+    last_accessed: Mapped[int] = mapped_column("last_accessed", nullable=False)
+    account_creation: Mapped[int] = mapped_column("account_creation", nullable=False)
     username: Mapped[str]
     message: Mapped[str]
     mii: Mapped[str]
     joinable: Mapped[bool]
-    game_description: Mapped[str] = mapped_column("gameDescription", nullable=False)
-    last_online: Mapped[int] = mapped_column("lastOnline", nullable=False)
-    favorite_game: Mapped[int] = mapped_column("jeuFavori", nullable=False)
+    game_description: Mapped[str] = mapped_column("game_description", nullable=False)
+    last_online: Mapped[int] = mapped_column("last_online", nullable=False)
+    favorite_game: Mapped[int] = mapped_column("jeu_favori", nullable=False)
 
 
 class DiscordFriends(Base):
-    __tablename__ = "discordFriends"
+    __tablename__ = "discord_friends"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    friend_code: Mapped[str] = mapped_column("friendCode", primary_key=True, nullable=False)
+    friend_code: Mapped[str] = mapped_column("friend_code", primary_key=True, nullable=False)
     network: Mapped[NetworkType] = mapped_column("network", NetworkTypeValue())
     active: Mapped[bool] = mapped_column(nullable=False)
 
@@ -69,15 +69,15 @@ class DiscordFriends(Base):
 class Discord(Base):
     __tablename__ = "discord"
 
-    id: Mapped[int] = mapped_column("ID", primary_key=True, nullable=False, unique=True)
+    id: Mapped[int] = mapped_column("id", primary_key=True, nullable=False, unique=True)
     refresh: Mapped[str] = mapped_column("refresh", nullable=False)
     bearer: Mapped[str] = mapped_column("bearer", nullable=False)
     session: Mapped[str] = mapped_column("session")
     token: Mapped[str] = mapped_column("token", unique=True)
-    last_accessed: Mapped[int] = mapped_column("lastAccessed", nullable=False)
-    generation_date: Mapped[int] = mapped_column("generationDate", nullable=False)
-    show_profile_button: Mapped[bool] = mapped_column("showProfileButton", nullable=False, default=True)
-    show_small_image: Mapped[bool] = mapped_column("showSmallImage", nullable=False, default=True)
+    last_accessed: Mapped[int] = mapped_column("last_accessed", nullable=False)
+    generation_date: Mapped[int] = mapped_column("generation_date", nullable=False)
+    show_profile_button: Mapped[bool] = mapped_column("show_profile_button", nullable=False, default=True)
+    show_small_image: Mapped[bool] = mapped_column("show_small_image", nullable=False, default=True)
 
 
 def start_db_time(uptime: Optional[datetime.datetime], network_type: NetworkType):
