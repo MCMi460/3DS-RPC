1edbfa40f2b690963ce8fc36148880659e6a0fc8
Spotlight <spotlight@joscomputing.space>
2024-09-23 00:27:39 -0500

    Add Pretendo friend code

--- api/public.py
+++ api/public.py
@@ -1,2 +1,4 @@
-pretendoBotFC = str(0).zfill(12) # FC == Friendcode ... not ready for prod yet
-nintendoBotFC = str(233790548638).zfill(12) # FC == Friendcode
+# Friend codes for 3dsrpc.com
+# TODO: This will become dynamic once multi-console support is available.
+pretendoBotFC = str(242170380419).zfill(12)
+nintendoBotFC = str(233790548638).zfill(12)
